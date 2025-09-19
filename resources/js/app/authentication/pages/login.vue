e587656ea556b5ec42d24b2399021c9dabd47ad9
mahi bashar akash <mahibashar2000002@gmail.com>
2025-09-20 00:09:16 +0600

    build

--- resources/js/app/authentication/pages/login.vue
+++ resources/js/app/authentication/pages/login.vue
@@ -19,10 +19,10 @@
             <div class="mt-2 w-full block text-red-500 text-xs font-medium" v-if="error?.password"> {{error?.password[0]}} </div>
         </div>
         <div class="mb-5 w-full block">
-            <button type="submit" class="min-w-[150px] text-sm font-medium bg-blue-400 duration-500 hover:bg-blue-700 text-white px-10 rounded-md min-h-[45px] max-h-[45px] cursor-pointer" v-if="!loading">
+            <button type="submit" class="min-w-[110px] text-sm font-medium bg-blue-400 duration-500 hover:bg-blue-700 text-white px-10 rounded-md min-h-[45px] max-h-[45px] cursor-pointer" v-if="!loading">
                 Login
             </button>
-            <button type="button" class="min-w-[150px] text-sm font-medium bg-blue-400 duration-500 hover:bg-blue-700 text-white px-10 rounded-md min-h-[45px] max-h-[45px] cursor-pointer" v-if="loading">
+            <button type="button" class="min-w-[110px] text-sm font-medium bg-blue-400 duration-500 hover:bg-blue-700 text-white px-10 rounded-md min-h-[45px] max-h-[45px] cursor-pointer" v-if="loading">
                 <span class="inline-block rounded-full w-4 h-4 border-2 border-white border-t-transparent animate-spin"></span>
             </button>
         </div>
